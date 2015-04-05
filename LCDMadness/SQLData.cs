5a37dd807f00cdaeb51d0055e5c0dc837aafa75f
smc1138 <smcogley@yahoo.com>
2015-04-05 08:52:57 -0700

    SQL connection string added to app.config and generating a new SQLData class for returning data objects

---
+++ LCDMadness/SQLData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace LCDMadness
+{
+    class SQLData
+    {
+        //http://azure.microsoft.com/en-us/documentation/articles/sql-database-dotnet-how-to-use/
+        //SqlConnectionStringBuilder csBuilder;
+        //csBuilder = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        ////After you have built your connection string, you can use the SQLConnection class to connect the SQL Database server:
+        //SqlConnection conn = new SqlConnection(csBuilder.ToString());
+        //conn.Open();
+    }
+}
